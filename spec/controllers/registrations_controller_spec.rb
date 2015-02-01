e03ed8db262095858a5184c96d6f0e55ed2d4d2e
Jakub Jatczak <j.jatczak@binarapps.com>
2015-02-01 19:40:21 +0100

    [fix] registration and session tests fix

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -10,10 +10,12 @@ describe RegistrationsController do
   describe 'POST create /users' do
 
     it 'register new user' do
-      post :create, { user: {login: 'email@example.com', password: 'password'}}, { 'Accept' => 'application/json' , 'Content-Type' => 'application/json' }
-      expect(response.status).to eq(201)
+      post :create, { user: {email: 'test@test.com', password: 'test123123', password_confirmation: 'test123123'}}, { 'Accept' => 'application/json' , 'Content-Type' => 'application/json' }
+      expect(response.status).to eq(200)
       json = JSON.parse(response.body)
       expect(json['state']['code']).to eq(0)
+      expect(json['data']['authentication_token']).to be_present
+      expect(json['data']['quota']).to eq(0)
       expect(User.all.count).to eq 1
     end
   end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,47 +11,27 @@ describe SessionsController do
   describe 'POST create /users/sign_in' do
 
     it 'properly authenticates user' do
-      post :create, { user: {login: 'email@example.com', password: 'password'}}, { 'Accept' => 'application/json' , 'Content-Type' => 'application/json' }
+      post :create, user: {email: 'email@example.com', password: 'password'}, format: :json
       expect(response.status).to eq(200)
-      assigns(:user).should be(@user)
-      #json = JSON.parse(response.body)
-      # expect(resp['success']).to  eq(true)
-      # expect(resp['info']).to  eq('Logged in')
-      # expect(resp['data']['email']).to  eq('email@example.com')
+      @user.reload
+      expect(assigns(:user)).to eq(@user)
+      json = JSON.parse(response.body)
+      expect(json['authentication_token']).to be_present
     end
 
     it 'shows errors when trying to provide incorrect data' do
-      post :create, { user: {login: 'email@exaaaample.com', password: 'password'}}, { 'Accept' => 'application/json' , 'Content-Type' => 'application/json' }
+      post :create, user: {email: 'email@example.com', password: 'wrong_password'}
       expect(response.status).to eq(401)
     end
-
   end
 
-  # describe 'DELETE destroy' do
-
-  #   it 'properly resets user auth token' do
-  #     user_old_token = @user.authentication_token
-  #     add_headers_to_request(@user.email, @user.authentication_token)
-  #     resp = logout_user(@user.email, @user.authentication_token)
-
-  #     expect(response.status).to eq(200)
-  #     expect(resp['success']).to  eq(true)
-  #     expect(resp['info']).to  eq("Logged out")
-
-  #     user_new_token = User.last.authentication_token
-  #     expect(user_new_token).not_to eq(user_old_token)
-  #   end
-
-  #   it "should not reset user token when passing improper headers " do
-  #     user_old_token = @user.authentication_token
-  #     add_headers_to_request("some@email.com", 'some_token')
-  #     #resp = logout_user(@user.email, @user.authentication_token)
-  #     delete :destroy, {}, { 'Accept' => 'application/json' , 'Content-Type' => 'application/json'}
-  #     JSON.parse(response.body)
-
-  #     expect(response.status).to eq(401)
-  #     expect(resp['success']).to  eq(false)
-  #   end
-  # end
-
+  describe 'DELETE destroy' do
+    it 'should properly sign user out' do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @user = User.create email: 'test@test.com', password: 'test123123', password_confirmation: 'test123123'
+      sign_in @user
+      delete :destroy, format: :json
+      expect(response.status).to eq(200)
+    end
+  end
 end
